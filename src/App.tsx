c8d481ef4ef1110ce3993f47211ea6253eda6845
ancibordaniil <130238809+ancibordaniil@users.noreply.github.com>
2025-03-04 19:05:39 +0300

    some changes in Panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Info from "./components/Info/Info";
+import Panels from "./components/Panels/Panels";
 
 const App: React.FC = () => {
     return (
         <>
             <Header />
             <Info />
+            <Panels />
         </>
     );
 }
